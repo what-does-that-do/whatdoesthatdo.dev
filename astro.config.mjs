9352464e8272e88da574f0b691359dd208071218
Oli <147245351+what-does-that-do@users.noreply.github.com>
2026-06-12 00:41:52 +0100

    Added sitemap generator and its' config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()],
+  site: "https://whatdoesthatdo.dev",
+  integrations: [react(), sitemap()],
 });
